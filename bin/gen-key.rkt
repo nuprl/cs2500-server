6b9f92bdc2d27736bae2e023455c98b42ab656b2
Leif Andersen <leif@leifandersen.net>
2016-08-02 14:24:27 -0400

    And should also be the name of the file as well.

--- bin/gen-key.rkt
+++ bin/gen-key.rkt
@@ -7,12 +7,13 @@
 (define-runtime-path student-server (build-path ".." "student-server"))
 (define-runtime-path grades-server (build-path ".." "grades-server"))
 
-(system* (find-executable-path "openssl")
-         "req" "-x509" "-newkey" "rsa:2048"
-         "-keyout" "private-key.pem" "-out" "server-cert.pem" "-days" "365")
-
-(copy-file (build-path this-dir "private-key.pem") student-server #t)
-(copy-file (build-path this-dir "private-key.pem") grades-server #t)
-(copy-file (build-path this-dir "server-cert.pem") student-server #t)
-(copy-file (build-path this-dir "server-cert.pem") grades-server #t)
+(void
+ (system* (find-executable-path "openssl")
+          "req" "-x509" "-newkey" "rsa:2048"
+          "-keyout" "private-key.pem" "-out" "server-cert.pem" "-days" "365"))
+ 
+(copy-file (build-path this-dir "private-key.pem") (build-path student-server "private-key.pem") #t)
+(copy-file (build-path this-dir "private-key.pem") (build-path grades-server "private-key.pem") #t)
+(copy-file (build-path this-dir "server-cert.pem") (build-path student-server "server-cert.pem") #t)
+(copy-file (build-path this-dir "server-cert.pem") (build-path grades-server "server-cert.pem") #t)
 (displayln "Remember to copy *only* server-cert.pem to the clients and update them")
