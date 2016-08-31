6dbb06f450782b20d9d1bee8a4b9c4fb8cddf4a4
Leif Andersen <leif@leifandersen.net>
2016-08-31 16:48:37 -0400

    Update the script to unzip files

--- bin/return_grades_to_students.rkt
+++ bin/return_grades_to_students.rkt
@@ -2,6 +2,7 @@
 #lang racket
 
 (require racket/cmdline
+         file/unzip
          "utils/constants.rkt"
          "utils/data-parse.rkt")
 
@@ -14,25 +15,58 @@
   (error 'part-count "Part count not positive integer: ~a" part-count))
 
 (define grader-assignment-dir (build-path grader-server-dir assignment))
+(define grader-assignment-backup-dir
+  (build-path grader-server-dir(format "~a-backup" assignment)))
 (define student-return-dir
   (build-path student-server-dir (format "~a-grades" assignment)))
 
 (unless (directory-exists? grader-assignment-dir)
   (error 'submission->grader
-         "assignment ~a does not exist in grader-server"
+         "assignment ~a does not exist in grades-server"
          assignment))
 
 (unless (directory-exists? student-return-dir)
   (error 'submission->grader
-         "assignment ~a does not exist in grader-server"
+         "assignment ~a does not exist in student-server"
          assignment))
 
-(for ([grader-path (directory-list grader-assignment-dir
-                                   #:build? #t)])
-  (define grader (last (explode-path grader-path)))
-  (unless (ormap (curry equal? grader) server-ignore-file-list)
-    (for ([student-path (directory-list grader-path
-                                        #:build? #t)])
-      (define student (last (explode-path student-path)))
-      (define return-path (build-path student-return-dir student))
-      (copy-directory/files student-path return-path))))
+;; Make a backup of directory before we begin
+(delete-directory/files grader-assignment-backup-dir #:must-exist? #f)
+(copy-directory/files grader-assignment-dir grader-assignment-backup-dir)
+
+; Do the copying for every grader
+(with-handlers ([exn:fail?
+                 (λ (e)
+                   ; Replace folder with backup
+                   (displayln (~a "Error occurred, reverting lab folder to its"
+                                  "original state"))
+                   (delete-directory/files grader-assignment-dir #:must-exist? #f)
+                   (copy-directory/files grader-assignment-backup-dir grader-assignment-dir)
+                   
+                   ; Re-raise exception
+                   (raise e))])
+  (parameterize ([current-directory grader-assignment-dir])
+    (for ([grader (directory-list grader-assignment-dir)])
+      (unless (ormap (curry equal? grader) server-ignore-file-list)
+        
+        ;; Unzip the file
+        (unless (file-exists? (build-path grader "grades.zip"))
+          (error 'grader "Grader ~a didn't return a grades.zip file" grader))
+        (delete-directory/files "grades.zip" #:must-exist? #f)
+        (rename-file-or-directory (build-path grader "grades.zip") "grades.zip")
+        (delete-directory/files grader #:must-exist? #f)
+        (call-with-unzip
+         "grades.zip"
+         (λ (dir)
+           (define tmp-folder (build-path dir grader))
+           (if (directory-exists? tmp-folder)
+               (copy-directory/files tmp-folder grader)
+               (error 'grader "Grader ~a is missing their grade folder" grader))))
+        (rename-file-or-directory "grades.zip" (build-path grader "grades.zip"))
+        
+        ;; Return to students
+        (for ([student-path (directory-list grader
+                                            #:build? #t)])
+          (define student (last (explode-path student-path)))
+          (define return-path (build-path student-return-dir student))
+          (copy-directory/files student-path return-path))))))
