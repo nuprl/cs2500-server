3b5ecdfceffb969ceab7d3ef10544932b8fb1a5a
Leif Andersen <leif@leifandersen.net>
2016-09-08 16:51:47 -0400

    Check multiple pair files

--- student-server/checker-lib.rkt
+++ student-server/checker-lib.rkt
@@ -1,7 +1,11 @@
 #lang racket/base
 
-(provide current-pairs-file
+(provide current-pairs-file-list
          valid-pairing)
+
+(define pairs-files
+  '("pairs1.rktd"
+    "pairs2.rktd"))
 
 (define (error* fmt . args)
   (error (apply format fmt args)))
@@ -10,11 +14,17 @@
   (with-input-from-file pairs-file
     (λ () (read))))
 
-(define current-pairs-file (make-parameter (build-path (current-directory) "pairs.rktd")))
+(define current-pairs-file-list
+  (make-parameter
+   (for/list ([i (in-list pairs-files)])
+     (build-path (current-directory) i))))
 
-(define (valid-pairing usernames)
+(define (valid-pairing/file usernames pair-file)
   (define sorted-usernames (if (list? usernames) usernames (list usernames)))
-  (unless (member usernames (get-pairs (current-pairs-file)))
+  (unless (member usernames (get-pairs pair-file))
     (if (list? usernames)
         (error* "Users not registered to work together: ~a" usernames)
         (error* "User ~a is not registered to work alone" usernames))))
+
+(define (valid-pairing usernames)
+  (map valid-pairing/file (current-pairs-file-list)))
