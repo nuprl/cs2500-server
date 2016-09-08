c411aacb347cec737ce527f50aaa8f398713c3c5
Leif Andersen <leif@leifandersen.net>
2016-09-08 15:04:11 -0400

    Error in make-assignment script

--- bin/make-assignment.rkt
+++ bin/make-assignment.rkt
@@ -19,7 +19,7 @@
 (unless (and (integer? part-count) (> part-count 0))
   (error 'part-count "Part count not positive integer: ~a" part-count))
 
-(for ([i (in-range 1 (add1 part-count))])
+(for ([i (add1 part-count)])
   (define assignment-path 
    (build-path student-server-dir
                (format "~a~a" assignment (integer->char (+ (char->integer #\a) i)))))
