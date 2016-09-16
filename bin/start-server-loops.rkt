48885829befd17435e95982755fac1d5b68b513f
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-09-16 09:49:11 -0400

    Remove redundant logging

--- bin/start-server-loops.rkt
+++ bin/start-server-loops.rkt
@@ -10,18 +10,15 @@
    (lambda ()
      (parameterize ([current-directory server-dir]
                     [current-subprocess-custodian-mode 'kill])
-       (call-with-output-file* "log.txt"
-         (lambda (log-file)
-           (let loop ()
-             (match-define (list _ _ pid _ callback)
-               (process*/ports log-file
-                               (current-input-port)
-                               'stdout
-                               (find-exe) "-I" "handin-server"))
-             (sleep (* 60 10)) ; 10 minutes
-             (callback 'kill)
-             (loop)))
-         #:exists 'append)))))
+       (let loop ()
+         (match-define (list _ _ pid _ callback)
+           (process*/ports (current-output-port)
+                           (current-input-port)
+                           'stdout
+                           (find-exe) "-I" "handin-server"))
+         (sleep (* 60 10)) ; 10 minutes
+         (callback 'kill)
+         (loop))))))
 
 (run-server-loop student-server-dir)
 (run-server-loop grader-server-dir)
