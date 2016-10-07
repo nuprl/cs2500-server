eabbce07295ec20ea0d612d1ff7073eb0151ce46
Leif Andersen <leif@leifandersen.net>
2016-10-07 13:13:14 -0400

    Update checker script

--- data/checker.rkt
+++ data/checker.rkt
@@ -2,11 +2,11 @@
 
 (require "../checker-lib.rkt")
 
-(check: :eval? #f
+(check: :eval? #t
         :users valid-pairing
         :create-text? #t
         :textualize? #t
-        :maxwidth 102
+        :maxwidth 80
         :student-line "Student: {username} Lab: {Lab} Grader: {Grader}"
         :extra-lines '("Maximum points for this assignment: <+CHANGEME>")
         :output "handin.rkt")
